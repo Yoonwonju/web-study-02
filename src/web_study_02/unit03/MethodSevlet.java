7d211f03488bb9dd002b517d2454e1dab887aa7c
yoon <lrzrlove@naver.com>
2020-08-18 11:19:35 +0900

    method 처리방식 완료

---
+++ src/web_study_02/unit03/MethodSevlet.java
@@ -0,0 +1,41 @@
+package web_study_02.unit03;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/MethodSevlet")
+public class MethodSevlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("method", "get");
+		processRequest(request, response);
+		
+//		response.setContentType("text/html; charset=UTF-8");
+//		response.getWriter()
+//		.append("<h1>get 방식으로 처리됨</h1>")
+//		.append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setAttribute("method", "post");
+		processRequest(request, response);
+	}
+
+	private void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//		response.setContentType("text/html; charset=UTF-8");
+//		response.getWriter()
+//		.append("<h1>post 방식으로 처리됨</h1>")
+//		.append(request.getContextPath());
+		RequestDispatcher dispatcher = request.getRequestDispatcher("result.jsp");
+		dispatcher.forward(request, response);
+		
+	}
+
+}
