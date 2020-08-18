5c66352602677207ef9c75c971a14ff15d48076d
yoon <lrzrlove@naver.com>
2020-08-18 10:42:50 +0900

    servelet 생명주기 확인

--- src/web_study_02/ServeletLifeCycle.java
+++ src/web_study_02/ServeletLifeCycle.java
@@ -15,42 +15,27 @@ import javax.servlet.http.HttpServletResponse;
 public class ServeletLifeCycle extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public ServeletLifeCycle() {
-        super();
-        // TODO Auto-generated constructor stub
+    	super();
+    	System.out.println("ServeletLifeCycle()");
     }
 
-	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
 	public void init(ServletConfig config) throws ServletException {
-		// TODO Auto-generated method stub
+		System.out.println("init()");
 	}
 
-	/**
-	 * @see Servlet#destroy()
-	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
+		System.out.println("destroy()");
 	}
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		System.out.println("doGet()");
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		System.out.println("doPost()");
+//		doGet(request, response);
 	}
 
 }
